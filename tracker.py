c801879593002952a4534f8aba6b6995f6d69cc5
Reo Yamanaka <ryamanaka807@gmail.com>
2021-06-10 13:34:16 -0400

    added main function and interactive feature

--- tracker.py
+++ tracker.py
@@ -1,11 +1,11 @@
 from datetime import datetime
 import csv
+from pprint import pprint
 
 def getLimit():
     with open("contributions.csv") as rf:
         csvFile = list(csv.reader(rf))
-        totalRows = len(csvFile)-1
-        print(csvFile)
+        totalRows = len(csvFile)-1 
         currentLimit = float(csvFile[totalRows][2])
     return currentLimit
 
@@ -17,7 +17,30 @@ def enterContribution(amount, addLimit = 0, overwriteLimit = False):
         else:
             recordFile.write(f"{datetime.today().strftime('%Y-%m-%d')},{amount},{getLimit()-amount+addLimit}\n")
 
-
-enterContribution(9999, overwriteLimit = True)
-
-
+def main():  
+    while True:
+        confirmation = input("Would you like to make a contribution today? [Y/N]: ")
+        if confirmation.lower() == "y":
+            while True:
+                update = input("Would you like to update your contribution limit first? [Y/N]: ")
+                if update.lower() == "y":
+                    updateOption = input("1) Add an amount to current contribution limit\n2)Overwrite and update total contribution limit\n")
+                    while True:
+                        if updateOption == "1":
+                            addLimit = float(input("Enter limit increase amount: "))
+                            break
+                        elif updateOption == "2":
+                            overwriteLimit = True
+                            break
+                elif update.lower() == "n":
+                    addLimit = 0
+                    overwriteLimit = False
+                    break
+            contributionAmount = float(input("How much are you contributing? "))
+            enterContribution(contributionAmount, addLimit = addLimit, overwriteLimit = overwriteLimit)
+            break
+        elif confirmation.lower() == "n":
+            print("\nI see. See you next time.\n")
+            break
+if __name__ == "__main__":
+    main()
